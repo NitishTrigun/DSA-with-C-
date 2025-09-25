9f07ee131f4a1ff8982b262161bcd1de96563c0b
Nitish Trigun <nitishtrigun5@gmail.com>
2025-09-25 23:31:25 +0530

    Leetcode problem

---
+++ leetcode442.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath> // for abs()
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.push_back(index + 1);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {4,3,2,7,8,2,3,1};
+
+    vector<int> duplicates = sol.findDuplicates(nums);
+
+    cout << "Duplicates: ";
+    for (int num : duplicates) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
